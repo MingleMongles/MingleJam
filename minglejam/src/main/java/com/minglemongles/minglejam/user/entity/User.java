439bda5b245228c6de32b615f5d57549c8a5b8ac
이효진 <jin12lee@naver.com>
2025-06-04 23:06:33 +0900

    feat: user 파일 구조

---
+++ minglejam/src/main/java/com/minglemongles/minglejam/user/entity/User.java
@@ -0,0 +1,10 @@
+package com.minglemongles.minglejam.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+// @Data
+public class User {
+}
