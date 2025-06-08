ee4b697295e3df4c9aea885c24535f94af63c69e
이효진 <jin12lee@naver.com>
2025-06-08 21:23:28 +0900

    feat: user entity 생성

--- minglejam/src/main/java/com/minglemongles/minglejam/user/entity/User.java
+++ minglejam/src/main/java/com/minglemongles/minglejam/user/entity/User.java
@@ -1,10 +1,50 @@
 package com.minglemongles.minglejam.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.data.annotation.Id;
+import lombok.*;
+
+import java.sql.Timestamp;
+
 
 @Entity
 @Table(name = "tb_user")
-// @Data
+@Getter
+@Setter
+@ToSring
 public class User {
+    @Id
+    @GeneratedValue(Strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_gender")
+    private String userGender;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "user_password")
+    private String userPWD;
+
+    @Column(name = "user_nickname")
+    private String userNickName;
+
+    @Column(name = "user_flag")
+    private Boolean userState;
+
+    @Column(name = "user_dormant_status")
+    private String userDormantState;
+
+    @Column(name = "created_at")
+    private Timestamp userCreated;
+
+    @Column(name = "updated_at")
+    private Timestamp userUpdated;
+
+
 }
