562740693c90745d979355ea4e9409fd4b8d5be0
jinjin <jin12lee@naver.com>
2025-06-09 22:50:58 +0900

    feat: user entity

--- minglejam/src/main/java/com/minglemongles/minglejam/user/entity/User.java
+++ minglejam/src/main/java/com/minglemongles/minglejam/user/entity/User.java
@@ -1,49 +1,63 @@
 package com.minglemongles.minglejam.user.entity;
 
 import jakarta.persistence.*;
-import org.springframework.data.annotation.Id;
+import java.util.UUID;
 import lombok.*;
-
 import java.sql.Timestamp;
-
 
 @Entity
 @Table(name = "tb_user")
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
-@ToSring
+@Builder
 public class User {
     @Id
-    @GeneratedValue(Strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
+    @Column(name = "user_id", columnDefinition = "VARCHAR(36)")
     private String userId;
 
-    @Column(name = "user_name")
+    @Column(name = "user_name", nullable = false)
     private String userName;
 
-    @Column(name = "user_gender")
-    private String userGender;
+    @Column(name = "user_gender", nullable = false)
+    private String userGender;  // 성병 enum 관리 수정 고민 중
 
-    @Column(name = "user_email")
+    @Column(name = "user_email", nullable = false)
     private String userEmail;
 
-    @Column(name = "user_password")
+    @Column(name = "user_password", nullable = false)
     private String userPWD;
 
-    @Column(name = "user_nickname")
+    @Column(name = "user_nickname", nullable = false)
     private String userNickName;
 
-    @Column(name = "user_flag")
+    @Column(name = "user_flag", nullable = false)
     private Boolean userState;
 
-    @Column(name = "user_dormant_status")
-    private String userDormantState;
+    @Column(name = "user_dormant_status", nullable = false)
+    private Boolean dormantState;
 
-    @Column(name = "created_at")
-    private Timestamp userCreated;
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
 
     @Column(name = "updated_at")
-    private Timestamp userUpdated;
+    private Timestamp updatedAt;
 
-
+    @PrePersist
+    public void uuid() {
+        if (this.userId == null) {
+            this.userId = UUID.randomUUID().toString();
+        }
+        if (userState == null) userState = true;
+        if (dormantState == null) dormantState = true;
+        if (this.createdAt == null) {
+            this.createdAt = new Timestamp(System.currentTimeMillis());
+        }
+        this.updatedAt = this.createdAt;
+        }
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
+    }
 }
